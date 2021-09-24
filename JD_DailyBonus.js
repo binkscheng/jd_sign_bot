2f8fe106f7cc65b166e66550357f6d25b9323c2f
binkscheng <35749724+binkscheng@users.noreply.github.com>
2021-09-24 15:48:20 +0800

    Update JD_DailyBonus.js

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -49,7 +49,7 @@ var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使�
 // var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 var OtherKey = `[{
-  "cookie":  "\""+process.env.JD_COOKIE+"\""
+   "cookie":  "`+process.env.JD_COOKIE+`"
 }]`
 
 /*以下样例为双账号("cookie"为必须,其他可选), 第一个账号仅包含Cookie, 第二个账号包含Cookie和金融签到Body: 
